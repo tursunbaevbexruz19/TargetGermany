f23870761366a843359ea55e941a24f0d2b82dd2
Bexruz <bexruzbek263@gmail.com>
2026-03-26 00:08:31 +0500

    fix: disable locale detection to force english default

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,14 +2,9 @@ import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ['en', 'de', 'uz', 'ru', 'es', 'ar', 'zh', 'tr', 'ko'],
-
-  // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localeDetection: false // Disable auto-language detection to force English
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation(routing);
+export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
